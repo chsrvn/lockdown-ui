f247a959ad870d4f4be36c5f9d63e27c853f30d8
Achint Kaur <achintk811@gmail.com>
2020-05-03 19:24:33 +0530

    ld-button initialized

---
+++ src/app/button/ld-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector:"ld-button",
+    templateUrl: "./ld-button.component.html",
+    styleUrls:["./ld-button.component.css"]
+})
+export class LdButtonComponent{
+
+}
---
+++ src/app/button/ld-button.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { LdButtonComponent } from './ld-button.component';
+
+@NgModule({
+    declarations:[LdButtonComponent],
+    exports:[LdButtonComponent]
+
+})
+export class ldButtonModule{
+
+}
